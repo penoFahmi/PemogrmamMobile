ae24c53484834a35ec9133b5bed5e82b5f3fdbd3
penoFahmi <penofahmi@gmail.com>
2024-11-07 18:38:24 +0700

    push

--- TugasKuisKota/app/src/main/java/com/peno/tugaskuiskota/MainActivity.java
+++ TugasKuisKota/app/src/main/java/com/peno/tugaskuiskota/MainActivity.java
@@ -1,24 +1,50 @@
 package com.peno.tugaskuiskota;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox pontianakCB;
+    private CheckBox majalengkaCB;
+    private CheckBox singkawangCB;
+    private CheckBox melawiCB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Inisialisasi CheckBox
+        pontianakCB = findViewById(R.id.pontianakCB);
+        majalengkaCB = findViewById(R.id.majalengkaCB);
+        singkawangCB = findViewById(R.id.singkawangCB);
+        melawiCB = findViewById(R.id.melawiCB);
+    }
+
+    public void submit(View view) {
+        String jawaban = "Pilihan yang benar di Kalimantan Barat:\n";
+
+        if (pontianakCB.isChecked()) {
+            jawaban += "- Pontianak\n";
+        }
+        if (singkawangCB.isChecked()) {
+            jawaban += "- Singkawang\n";
+        }
+        if (melawiCB.isChecked()) {
+            jawaban += "- Melawi\n";
+        }
+
+        if (!pontianakCB.isChecked() && !singkawangCB.isChecked() && !melawiCB.isChecked()) {
+            jawaban = "Tidak ada jawaban yang benar yang dipilih.";
+        }
+
+        Toast.makeText(this, jawaban, Toast.LENGTH_LONG).show();
     }
 }
